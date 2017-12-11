28ad388d451c875b8aa1041d1eab98149c01a29e
Crista Cooper <aine3d@gmail.com>
2017-12-11 14:25:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,10 +10,11 @@ class School
 
   def add_student(student, grade)
     if @roster.key?(grade)
-    @roster[grade] << student
-  else
-    @roster[grade] = []
-    @roster[grade] << student
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
   end
 
   def grade(grades)
