176ee0286be4d9b235063e323b5c5f4affaf2d8a
Crista Cooper <aine3d@gmail.com>
2017-12-11 14:23:33 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+
+class School
+  attr_reader :roster, :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] << student
+  end
+
+  def grade(grades)
+    @roster[grades]
+  end
+
+  def sort
+    @roster.each {|k, v| v.sort!}
+  end
+
+end
